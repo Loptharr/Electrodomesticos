657ccff5f4b6169448e803ca77c972ba99517a49
marceloart <marcelocorreaart@gmail.com>
2020-02-28 21:33:32 -0300

    Inicios de Junit tests

---
+++ Electrodomesticos/src/Model/ElectrodomesticTest.java
@@ -0,0 +1,27 @@
+package Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ElectrodomesticTest {
+
+	@Test
+	void test_checkConsumption() {
+		Television t = new Television(); 
+		assertEquals(t._checkConsumption(Consumption._F), true);
+	}
+
+	@Test
+	void test_checkColor() {
+		Television t = new Television(); 
+		assertEquals(t._checkColor(Color._WHITE), true);
+	}
+
+	@Test
+	void test_finalPrice() {
+		Television t = new Television(); 
+		assertEquals(t._finalPrice(), 100020);
+	}
+
+}
